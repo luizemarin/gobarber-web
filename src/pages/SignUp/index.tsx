0535d9d926ce1f421b6cb88e7850876d47e9567d
luizemarin <luizemarin@gmail.com>
2020-07-06 16:54:39 -0300

    Create validation errors SignUp screen

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,25 +1,45 @@
 /* eslint-disable import/no-unresolved */
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 
 import { Container, Content, Background } from './style';
 import logoImage from '../../assets/logo.svg';
 
+import getValidationErrors from '../../utils/getValidationErrors';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const formRef = useRef<FormHandles>(null);
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string()
+          .required('Senha obrigatória')
+          .min(6, 'No mínimo 6 digitos'),
+      });
+
+      await schema.validate(data, { abortEarly: false });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
 
   return (
     <Container>
       <Background />
       <Content>
         <img src={logoImage} alt="GoBarber" />
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
           <Input name="name" icon={FiUser} placeholder="Nome" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
